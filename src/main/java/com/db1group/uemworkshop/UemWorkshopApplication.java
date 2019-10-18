8ede5589d900d6771be82f282643eb3ded8f5f15
Altieres de Matos <altieres.matos@db1.com.br>
2019-10-18 16:13:43 -0300

    First commit

---
+++ src/main/java/com/db1group/uemworkshop/UemWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.db1group.uemworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UemWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UemWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db1group/uemworkshop/UemWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db1group.uemworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UemWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
